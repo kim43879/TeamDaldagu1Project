cd587e8f60936c55bb4661f1c654684268823ee6
김희진 <hh80930@naver.com>
2024-08-20 11:29:23 +0900

    김희진: UserBean: annotation 추가

--- src/main/java/com/Daldagu1/TeamDaldagu1Project/beans/UserBean.java
+++ src/main/java/com/Daldagu1/TeamDaldagu1Project/beans/UserBean.java
@@ -1,5 +1,8 @@
 package com.Daldagu1.TeamDaldagu1Project.beans;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,14 +10,30 @@ import lombok.Setter;
 @Setter
 public class UserBean {
     private int user_idx;
+
+    @Size(min=2, max=10)
+    @Pattern(regexp = "[a-zA-Z0-9]*")
     private String user_id;
+
+    @Size(min=8, max=20)
+    @Pattern(regexp = "[a-zA-Z0-9!@#$%^&*()_+\\\\-=\\\\[\\\\]{};':\\\"\\\\\\\\|,.<>\\\\/?]*")
     private String user_pw;
+
+    @Size(min=2, max=10)
+    @Pattern(regexp = "[가-힣]*")
     private String user_name;
+
+    @Email
     private String user_email;
     private String user_phone;
     private char user_role;
     private char user_available;
     private int user_daily;
     private int user_membership_idx;
+
+    //DB 이외
+    @Size(min=8, max=20)
+    @Pattern(regexp = "[a-zA-Z0-9!@#$%^&*()_+\\\\-=\\\\[\\\\]{};':\\\"\\\\\\\\|,.<>\\\\/?]*")
+    private String user_pw2;
 
 }
